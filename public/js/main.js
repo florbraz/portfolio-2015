654b851d4daaa91770b084d03ed09e45106a9d53
Flor Braz <eu@florbraz.com>
2015-11-25 02:57:16 -0200

    Starting Routes

--- public/js/main.js
+++ public/js/main.js
@@ -10,30 +10,16 @@ define([
 	
 	app.config(function($routeProvider){
 		$routeProvider
-			.when('/:language?',
-				{redirectTo : ':language?/my-tasks/all'})
-			.when('/:language?/my-tasks/:status',
-				angularAMD.route(Routes.MyTasks))
-			.when('/:language?/my-team/tasks/:status',
-				angularAMD.route(Routes.MyTeam))
-			.when('/:language?/my-process/list/:status',
-				angularAMD.route(Routes.MyProcesses))
-			.when('/:language?/my-process/new',
-				angularAMD.route(Routes.NewProcess))
-			.when('/:language?/my-process/view',
-				angularAMD.route(Routes.ProcessViewList))
-			.when('/:language?/my-process/view/:id',
-				angularAMD.route(Routes.ProcessView))
-			.when('/:language?/process-templates/new',
-				angularAMD.route(Routes.ProcessTemplateCreate))
-			.when('/:language?/process-templates/view',
-				angularAMD.route(Routes.ProcessTemplateViewList))
-			.when('/:language?/process-templates/view/:id',
-				angularAMD.route(Routes.ProcessTemplateView))
-			.when('/:language?/process-templates/edit',
-				angularAMD.route(Routes.ProcessTemplateEditList))
-			.when('/:language?/process-templates/edit/:id',
-				angularAMD.route(Routes.ProcessTemplateEdit))
+			.when('/',
+				{redirectTo : '/'})
+			.when('/works',
+				angularAMD.route(Routes.Works))
+			.when('/work/:id',
+				angularAMD.route(Routes.Work))
+			.when('/blog',
+				angularAMD.route(Routes.Blog))
+			.when('/blog/:id',
+				angularAMD.route(Routes.BlogPost))
 			.otherwise(Routes.NotFound)
 	});
 			
